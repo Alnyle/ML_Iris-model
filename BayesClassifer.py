21d5103201cd964a1e6a077bf0ee8927d2708100
Omar-Salah123 <sd.omar04@gmail.com>
2024-11-01 17:35:15 +0200

    bababa
    bababa

---
+++ BayesClassifer.py
@@ -0,0 +1,11 @@
+from torch._C._te import __init__
+import scikit-learn
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
+
+class NaiveBayesClassifier:
+    def __init__(self,array):
+
